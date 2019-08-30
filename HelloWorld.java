7bc0ef17132ba5a9dc08b33bb40a0432663d16b3
vicky-sai <vicky.sss.1997@gmail.com>
2019-08-30 15:29:28 +0530

    second commit1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld	{
 
      public static void main(String []args) throws InterruptedException {
          Thread.sleep(2000);
-        System.out.println("Hello World");
+        System.out.println("======Hello World======");
      }
 }
